ccce510ac9498ea2ac8dbae849f0b341309781e4
CaimeoAI <judiellezinke@yahoo.de>
2025-02-07 21:25:02 +0100

    set up all the necessary useStates for the project

--- src/components/LoadMore.jsx
+++ src/components/LoadMore.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react"
 
 
 
 export default function LoadMore() {
+
+    const [loading, setLoading] = useState(false)
+    const [productsData, setProductsData] = useState([])
+    const [count, setCount] = useState(0)
+
     return (
         <div className="loadmore-wrapper">
             
