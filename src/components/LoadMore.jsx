38259d8225db9ae0de60e27c354d346fa3160d84
CaimeoAI <judiellezinke@yahoo.de>
2025-02-07 21:37:40 +0100

    added fetching of dummyData with skip being a variable tied to the count useState and added the useEffect that starts the fetch function upon creation of the component

--- src/components/LoadMore.jsx
+++ src/components/LoadMore.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { useState } from "react"
 
 
@@ -7,6 +8,23 @@ export default function LoadMore() {
     const [loading, setLoading] = useState(false)
     const [productsData, setProductsData] = useState([])
     const [count, setCount] = useState(0)
+
+    async function fetchProducts(){
+        try {
+            
+            const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`)
+            const result = await response.json()
+
+            console.log(res)
+
+        } catch(e) {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    },[])
 
     return (
         <div className="loadmore-wrapper">
